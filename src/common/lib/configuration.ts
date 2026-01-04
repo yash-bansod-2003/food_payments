35c15c156e6cbd3c9a89e997d26ff81705869135
yashbansod <yashbansodgit@gmail.com>
2026-01-05 01:07:01 +0530

    fix: replace parseInt with Number.parseInt for consistent type conversion

--- src/common/lib/configuration.ts
+++ src/common/lib/configuration.ts
@@ -4,10 +4,10 @@ config({ path: `.env.${process.env.NODE_ENV}` });
 const configuration = {
   node_env: process.env.NODE_ENV,
   host: process.env.HOST ?? "localhost",
-  port: parseInt(process.env.PORT ?? "80"),
+  port: Number.parseInt(process.env.PORT ?? "80"),
   database: {
     host: process.env.DATABASE_HOST,
-    port: parseInt(process.env.DATABASE_PORT ?? "5432"),
+    port: Number.parseInt(process.env.DATABASE_PORT ?? "5432"),
     user: process.env.DATABASE_USER,
     password: process.env.DATABASE_PASSWORD,
     database: process.env.DATABASE,
